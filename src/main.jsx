91d2b0b908a6f0d6f15029f06570731de846fa48
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-11-08 04:27:32 +0600

    Problem In Main JSX

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-// import ReactDOM from 'react-dom/client'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import ReactDOM from 'react-dom'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
+
 import Router from './Routes/Router/Router.jsx'
 import AuthProvider from './Providers/AuthProvider.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <RouterProvider router={Router} />
