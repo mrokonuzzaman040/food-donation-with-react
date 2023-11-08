24b79e2e4c5c66ecbe1aeb6a84eefe8f00f0e67b
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-11-08 21:51:48 +0600

    Add gap between table cells in ManageFood component

--- src/Components/ManageFood/ManageFood.jsx
+++ src/Components/ManageFood/ManageFood.jsx
@@ -81,7 +81,7 @@ const ManageFood = () => {
     return (
         <div className='max-w-7xl px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto'>
             <div className="overflow-x-auto w-full">
-                <table className="table w-full">
+                <table className="table w-full gap-2">
                     <thead>
                         <tr>
                             <th>
