88f675442a6f5833296daa62760e8c7ff723fc62
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-11-08 17:27:53 +0600

    Fix typos and add logo to brand in header and navbar, and update loading spinner in PrivetRoute

--- src/Components/Shared/Header/Footer/Footer.jsx
+++ src/Components/Shared/Header/Footer/Footer.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { FaFacebook, FaGithub, FaTwitter, FaYoutube } from 'react-icons/fa';
 
+import logo from '../../../../assets/logo.gif'
+
 const Footer = () => {
     return (
         <footer className="w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 mx-auto">
             <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6 mb-10">
                 <div className="col-span-full hidden lg:col-span-1 lg:block">
-                    <a className="flex-none text-xl font-semibold dark:text-white" href="#" aria-label="Brand">Extra Food</a>
+                    <a className="flex-none text-xl font-semibold dark:text-white" href="#" aria-label="Brand">Extra Food <span><img src={logo} alt="" /></span></a>
                     <p className="mt-3 text-xs sm:text-sm text-gray-600 dark:text-gray-400">© 2022 RokonUzzaman040.</p>
                 </div>
                 <div>
